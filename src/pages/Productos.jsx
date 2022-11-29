88bb383ce457b89eb7952806bdff1523af68e716
hurtado vallecilla <andresvallelap@gmail.com>
2022-11-28 19:51:04 -0500

    ahora si app lista

--- src/pages/Productos.jsx
+++ src/pages/Productos.jsx
@@ -12,7 +12,7 @@ const Productos = () => {
             <div className="star">
               <h2>LeBron 16</h2>
               <p>$185000</p>
-              <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/881020/nike02a.png" />
+              <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/881020/nike02a.png" alt="zapato nike"/>
             </div>
           </div>
         </li>
@@ -21,7 +21,7 @@ const Productos = () => {
             <div className="star">
               <h2>Nike Odyssey React Shield</h2>
               <p>$130000</p>
-              <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/881020/nike01a.png" />
+              <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/881020/nike01a.png" alt="zapato nike"/>
             </div>
           </div>
         </li>
@@ -30,7 +30,7 @@ const Productos = () => {
             <div className="star">
               <h2>Nike Epic React Flyknit</h2>
               <p>$150000</p>
-              <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/881020/nike03a.png" />
+              <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/881020/nike03a.png" alt="zapato nike"/>
             </div>
           </div>
         </li>
@@ -39,7 +39,7 @@ const Productos = () => {
             <div className="star">
               <h2>Nike Air Max 97 Premium</h2>
               <p>$180000</p>
-              <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/881020/nike04a.png" />
+              <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/881020/nike04a.png" alt="zapato nike"/>
             </div>
           </div>
         </li>
@@ -48,7 +48,7 @@ const Productos = () => {
             <div className="star">
               <h2>Nike Free RN Motion Flyknit</h2>
               <p>$150000</p>
-              <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/881020/nike05a.png" />
+              <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/881020/nike05a.png" alt="zapato nike"/>
             </div>
           </div>
         </li>
@@ -57,7 +57,7 @@ const Productos = () => {
             <div className="star">
               <h2>Nike Free RN Flyknit 2018</h2>
               <p>$120000</p>
-              <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/881020/nike06a.png" />
+              <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/881020/nike06a.png" alt="zapato nike"/>
             </div>
           </div>
         </li>
@@ -66,7 +66,7 @@ const Productos = () => {
             <div className="star">
               <h2>Nike Rise React Flyknit</h2>
               <p>$175000</p>
-              <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/881020/nike07a.png" />
+              <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/881020/nike07a.png" alt="zapato nike"/>
             </div>
           </div>
         </li>
@@ -75,7 +75,7 @@ const Productos = () => {
             <div className="star">
               <h2>Nike Flyknit Racer G</h2>
               <p>$175000</p>
-              <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/881020/nike08a.png" />
+              <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/881020/nike08a.png" alt="zapato nike"/>
             </div>
           </div>
         </li>
@@ -84,7 +84,7 @@ const Productos = () => {
             <div className="star">
               <h2>Nike EXP-X14 Premium iD</h2>
               <p>$160000</p>
-              <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/881020/nike09a.png" />
+              <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/881020/nike09a.png" alt="zapato nike"/>
             </div>
           </div>
         </li>
@@ -93,7 +93,7 @@ const Productos = () => {
             <div className="star">
               <h2>Nike Free x Retcon</h2>
               <p>$120000</p>
-              <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/881020/nike10a.png" />
+              <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/881020/nike10a.png" alt="zapato nike"/>
             </div>
           </div>
         </li>
@@ -102,7 +102,7 @@ const Productos = () => {
             <div className="star">
               <h2>Nike Air Force 1 '07 Suede</h2>
               <p>$90000</p>
-              <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/881020/nike11a.png" />
+              <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/881020/nike11a.png" alt="zapato nike"/>
             </div>
           </div>
         </li>
@@ -111,7 +111,7 @@ const Productos = () => {
             <div className="star">
               <h2>Nike Metcon 4 iD</h2>
               <p>$150000</p>
-              <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/881020/nike12a.png" />
+              <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/881020/nike12a.png" alt="zapato nike"/>
             </div>
           </div>
         </li>
@@ -120,7 +120,7 @@ const Productos = () => {
             <div className="star">
               <h2>LeBron 16</h2>
               <p>$185000</p>
-              <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/881020/nike02a.png" />
+              <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/881020/nike02a.png" alt="zapato nike"/>
             </div>
           </div>
         </li>
@@ -129,7 +129,7 @@ const Productos = () => {
             <div className="star">
               <h2>Nike Odyssey React Shield</h2>
               <p>$130000</p>
-              <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/881020/nike01a.png" />
+              <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/881020/nike01a.png" alt="zapato nike"/>
             </div>
           </div>
         </li>
@@ -138,7 +138,7 @@ const Productos = () => {
             <div className="star">
               <h2>Nike Epic React Flyknit</h2>
               <p>$150000</p>
-              <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/881020/nike03a.png" />
+              <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/881020/nike03a.png" alt="zapato nike"/>
             </div>
           </div>
         </li>
@@ -147,7 +147,7 @@ const Productos = () => {
             <div className="star">
               <h2>Nike Air Max 97 Premium</h2>
               <p>$180000</p>
-              <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/881020/nike04a.png" />
+              <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/881020/nike04a.png" alt="zapato nike"/>
             </div>
           </div>
         </li>
@@ -156,7 +156,7 @@ const Productos = () => {
             <div className="star">
               <h2>Nike Free RN Motion Flyknit</h2>
               <p>$150000</p>
-              <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/881020/nike05a.png" />
+              <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/881020/nike05a.png" alt="zapato nike"/>
             </div>
           </div>
         </li>
@@ -165,7 +165,7 @@ const Productos = () => {
             <div className="star">
               <h2>Nike Free RN Flyknit 2018</h2>
               <p>$120000</p>
-              <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/881020/nike06a.png" />
+              <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/881020/nike06a.png" alt="zapato nike"/>
             </div>
           </div>
         </li>
@@ -174,7 +174,7 @@ const Productos = () => {
             <div className="star">
               <h2>Nike Rise React Flyknit</h2>
               <p>$175000</p>
-              <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/881020/nike07a.png" />
+              <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/881020/nike07a.png" alt="zapato nike"/>
             </div>
           </div>
         </li>
@@ -183,7 +183,7 @@ const Productos = () => {
             <div className="star">
               <h2>Nike Flyknit Racer G</h2>
               <p>$175000</p>
-              <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/881020/nike08a.png" />
+              <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/881020/nike08a.png" alt="zapato nike"/>
             </div>
           </div>
         </li> 
